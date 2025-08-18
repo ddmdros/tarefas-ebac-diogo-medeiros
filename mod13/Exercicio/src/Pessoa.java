d63f0310680863a16be764b96721443b571cdc1a
ddmdros <diogome_deiros@hotmail.com>
2025-08-18 18:29:27 -0300

    criação classe abstrada

---
+++ mod13/Exercicio/src/Pessoa.java
@@ -0,0 +1,19 @@
+public abstract class Pessoa {
+
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                '}';
+    }
+}
