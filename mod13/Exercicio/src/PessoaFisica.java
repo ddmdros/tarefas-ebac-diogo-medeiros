422f2a28f17ded0f9e7598528ef35a6cbc6df771
ddmdros <diogome_deiros@hotmail.com>
2025-08-18 18:29:42 -0300

    criação subclasses

---
+++ mod13/Exercicio/src/PessoaFisica.java
@@ -0,0 +1,18 @@
+public class PessoaFisica extends Pessoa {
+    private String CPF;
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    @Override
+    public String toString() {
+        return "PessoaFisica{" +
+                "CPF='" + CPF + '\'' +
+                '}';
+    }
+}
---
+++ mod13/Exercicio/src/PessoaJuridica.java
@@ -0,0 +1,18 @@
+public class PessoaJuridica extends Pessoa {
+    private String CNPJ;
+
+    public String getCNPJ() {
+        return CNPJ;
+    }
+
+    public void setCNPJ(String CNPJ) {
+        this.CNPJ = CNPJ;
+    }
+
+    @Override
+    public String toString() {
+        return "PessoaJuridica{" +
+                "CNPJ='" + CNPJ + '\'' +
+                '}';
+    }
+}
